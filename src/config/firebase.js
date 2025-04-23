ac7041137079da76abb48cf5a472e7bd89d106e2
UrielP-Dev <kaattoo9@gmail.com>
2025-04-23 15:43:10 -0600

    merge conflicts with master

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -12,7 +12,7 @@ import {
 } from "@env";
 
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: FIREBASE_AUTH_DOMAIN,
   projectId: FIREBASE_PROJECT_ID,
   storageBucket: FIREBASE_STORAGE_BUCKET,
